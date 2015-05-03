11a4a8ab203a6bc7f3d3dbdbdb91187e39ffc998
davedoff <davidhalldoff@gmail.com>
2015-05-03 19:52:00 +0200

    Update Flashmessagetest.php

--- test/flash/Flashmessagetest.php
+++ test/flash/Flashmessagetest.php
@@ -1,1 +1,26 @@
+<?php
 
+namespace Dahc\Flashmessage;
+
+/**
+ * A testclass
+ * 
+ */
+class FlashmessageTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Test
+     *
+     * @return void
+     *
+     */
+    public function testError()
+    {
+        $flashmessage = new \dahc\Flashmessage\Flashmessage();
+
+        $res = $flashmessage->Error();
+        $exp = $this->addMessage('error', $content);
+        $this->assertEquals($res, $exp, "The name does not match.");
+    }
+}
